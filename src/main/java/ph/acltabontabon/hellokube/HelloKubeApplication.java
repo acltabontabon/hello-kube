df322a0e4c5dbf395e1237f6f7c486cd57818667
Alvin Cris Tabontabon <acltabontabon@gmail.com>
2021-07-31 22:56:29 +0800

    Test

---
+++ src/main/java/ph/acltabontabon/hellokube/HelloKubeApplication.java
@@ -0,0 +1,20 @@
+package ph.acltabontabon.hellokube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloKubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloKubeApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/ph/acltabontabon/hellokube/HelloKubeApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.acltabontabon.hellokube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloKubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
